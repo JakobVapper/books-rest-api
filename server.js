4ff496c0b21f1d284376e3a2292e154d4704b670
JakobVapper <jakob.vapper@ametikool.ee>
2023-10-26 11:42:35 +0300

    update

--- server.js
+++ server.js
@@ -10,10 +10,40 @@ app.get('/books', async (req, res) => {
     res.status(200).json(books);
 });
 
-app.get('/', (request, response) => {
-    response.send('hello world');
+app.get('/books/:id', async (req, res) => {
+    console.log(typeof req.params.id);
+
+    try{
+        const book = await prisma.books.findUnique({
+            where : {
+                id: Number(req.params.id),
+            }
+        });
+
+        res.status(200).json(book);
+    } catch(err){
+        res.status(404).send({
+            message: "Midagi läks valesti",
+            error,
+        })
+    }
+});
+
+app.delete('/books/:id', async (req, res) => {
+    try{
+        const deletedBook = await prisma.books.delete ({
+            where: {
+                id: Number(req.params.id),
+            }})
+    }catch(err){
+        res.status(404).send({
+            message: "Midagi läks valesti",
+            error,
+        })
+    }
 });
 
 app.listen(PORT, () => {
     console.log(`Server listening at port ${PORT}`);
 });
+
